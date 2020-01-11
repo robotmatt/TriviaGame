d26e631da6c1d4b848c2128d7ffa207f0c074fd5
Matt Martin <matt@incgn.to>
2020-01-10 20:36:02 -0600

    countdown fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,35 @@ let game = {
     incorrectAnswers: -1,
     unanswered: -1,
     time: -1,
+    questionIndex: -1,
     intervalId: ""
 }
 
-function displayQuestion(questionNumber) {
-    $("#question").text(questions[questionNumber].question);
-    return questionNumber;
+function endGame() {
+    clearInterval(game.intervalId);
+}
+
+function updateTime() {
+    if (game.questionIndex === questions.length) {
+        endGame();
+    } else if (game.time === 0) {
+        clearInterval(game.intervalId);
+        //  Alert the user that time is up.
+        alert("Time Up!");
+        nextQuestion();
+    } else {
+        $("#time-remaining").text("Time Remaining: " + game.time-- + " seconds");
+    }
+}
+
+function displayQuestion() {
+    $(".start-game").remove();
+    game.time = 5;
+    clearInterval(game.intervalId);
+    // Start a timer and update the timer value
+    game.intervalId = setInterval(updateTime, 1000);
+
+    $("#question").text(questions[game.questionIndex].question);
 }
 
 function resetGame() {
@@ -19,29 +42,21 @@ function resetGame() {
     game.time = -1;
 }
 
-function updateTime() {
-
-    $("#time-remaining").text("Time Remaining: " + --game.time + " seconds");
-    if (game.time === 0) {
-
-        clearInterval(game.intervalId);
-        //  Alert the user that time is up.
-        alert("Time Up!");
-    }
+function nextQuestion() {
+    clearInterval(game.intervalId);
+    game.questionIndex++;
+    displayQuestion();
 }
 
-function playGame() {
+
+function playGame(index) {
     resetGame();
     $(".start-game").remove();
-    questions.forEach(function (item, index) {
-        // Display the question
-        displayQuestion(index);
-        game.time = 5;
-        clearInterval(game.intervalId);
-        // Start a timer and update the timer value
-        game.intervalId = setInterval(updateTime, 1000);
-
-    })
+    game.time = 5;
+    game.questionIndex = 0;
+    clearInterval(game.intervalId);
+    // Start a timer and update the timer value
+    game.intervalId = setInterval(updateTime, 1000);
 }
 
 $(document).ready(function () {
@@ -49,8 +64,9 @@ $(document).ready(function () {
     $("#main").append($("<button>").addClass("start-game").text("Start Game"));
 
     $(document).on("click", ".start-game", function () {
-
         // Start the game
-        playGame();
+        //playGame(0);
+        game.questionIndex = 0;
+        displayQuestion();
     });
 });
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,5 +6,21 @@ const questions = [
         choice2: "wrong2",
         choice3: "wrong3",
         image: ""
+    },
+    {
+        question: "What a Question2",
+        answer: "This is an answer",
+        choice1: "wrong1",
+        choice2: "wrong2",
+        choice3: "wrong3",
+        image: ""
+    },
+    {
+        question: "What a Question3",
+        answer: "This is an answer",
+        choice1: "wrong1",
+        choice2: "wrong2",
+        choice3: "wrong3",
+        image: ""
     }
 ]
